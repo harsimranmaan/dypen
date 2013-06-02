d68c09be62f65fe6e371692c46bd065924ff418a
Harsimran Singh Maan <maan.harry@gmail.com>
2013-06-02 01:05:50 -0700

    Refactor

--- javarmi/StockExClient/src/stockexclient/InteractionManager.java
+++ javarmi/StockExClient/src/stockexclient/InteractionManager.java
@@ -25,19 +25,27 @@ public class InteractionManager
     private Client client;
     private boolean isAdmin;
 
+    /**
+     * Prints the login message to console
+     */
     private void printloginMessage()
     {
         System.out.println("Please log in before you send the request.");
     }
 
+    /**
+     * Prints the message to console
+     * <p/>
+     * @param message
+     */
     private void printMessage(String message)
     {
         System.out.println(message);
     }
 
     /**
-     *
-     * @return client type
+     * Checks if the user is loggedIn
+     * <p/>
      */
     private boolean isLoggedIn()
     {
@@ -45,7 +53,8 @@ public class InteractionManager
     }
 
     /**
-     *
+     * Prints a warning to console
+     * <p/>
      * @param commandName
      */
     private void printWarning(String commandName)
@@ -55,7 +64,8 @@ public class InteractionManager
     }
 
     /**
-     *
+     * Handles Interation with the User
+     * <p/>
      * @param stockQuery
      * @param auth
      * @param isAdmin
@@ -72,7 +82,241 @@ public class InteractionManager
     }
 
     /**
-     *
+     * Handles the query command
+     * <p/>
+     * @param commandString
+     */
+    private void query(String[] commandString)
+    {
+        if (isLoggedIn())
+        {
+            if (commandString.length == 2)
+            {
+                try
+                {
+                    System.out.println(stockQuery.query(client, commandString[1]));
+                }
+                catch (Exception ex)
+                {
+
+                    printMessage(ex.getMessage());
+                }
+            }
+            else
+            {
+                printWarning(commandString[0]);
+            }
+        }
+        else
+        {
+            printloginMessage();
+        }
+    }
+
+    /**
+     * Handles the list command
+     * <p/>
+     * @param commandString <p/>
+     * @throws RemoteException
+     */
+    private void list(String[] commandString) throws RemoteException
+    {
+        if (isLoggedIn())
+        {
+            if (commandString.length == 1 && !client.isAdmin())
+            {
+
+                List<Stock> list = stockQuery.list(client);
+                if (list.size() > 0)
+                {
+                    for (Stock stock : list)
+                    {
+                        System.out.println(stock);
+                    }
+                }
+                else
+                {
+                    printMessage("No records found.");
+                }
+
+
+            }
+            else
+            {
+                printWarning(commandString[0]);
+            }
+        }
+        else
+        {
+            printloginMessage();
+        }
+    }
+
+    /**
+     * Handles the buy command
+     * <p/>
+     * @param commandString
+     */
+    private void buy(String[] commandString)
+    {
+        int quantity;
+        if (isLoggedIn())
+        {
+            if (commandString.length == 3 && !client.isAdmin())
+            {
+
+                try
+                {
+                    quantity = getInteger(commandString[2]);
+                    if (quantity > 0)
+                    {
+                        client = stockQuery.buy(client, commandString[1], quantity);
+                        System.out.println(Integer.toString(quantity) + " " + commandString[1] + " bought.");
+                        System.out.println(" Your current balance is $" + client.getBalance() + " .");
+                    }
+                    else
+                    {
+                        System.out.println("Enter quantity greater than zero.");
+                    }
+                }
+                catch (NumberFormatException ne)
+                {
+                    printWarning(commandString[0]);
+                }
+                catch (Exception ex)
+                {
+                    printMessage(ex.getMessage());
+                }
+
+            }
+            else
+            {
+                printWarning(commandString[0]);
+            }
+        }
+        else
+        {
+            printloginMessage();
+        }
+    }
+
+    /**
+     * Handles the sell command
+     * <p/>
+     * @param commandString
+     */
+    private void sell(String[] commandString)
+    {
+        int quantity;
+        if (isLoggedIn())
+        {
+            if (commandString.length == 3 && !client.isAdmin())
+            {
+
+                try
+                {
+                    quantity = getInteger(commandString[2]);
+                    if (quantity > 0)
+                    {
+                        client = stockQuery.sell(client, commandString[1], quantity);
+                        System.out.println(Integer.toString(quantity) + " " + commandString[1] + " sold.");
+                        System.out.println(" Your current balance is $" + client.getBalance() + " .");
+                    }
+                    else
+                    {
+                        System.out.println("Enter quantity great than zero.");
+                    }
+                }
+                catch (NumberFormatException ne)
+                {
+                    printWarning(commandString[0]);
+                }
+                catch (Exception ex)
+                {
+                    printMessage(ex.getMessage());
+                }
+            }
+            else
+            {
+                printWarning(commandString[0]);
+            }
+        }
+        else
+        {
+            printloginMessage();
+        }
+    }
+
+    /**
+     * Handles the update command
+     * <p/>
+     * @param commandString
+     */
+    private void update(String[] commandString)
+    {
+        if (isLoggedIn())
+        {
+            if (commandString.length == 3 && client.isAdmin())
+            {
+                try
+                {
+                    double price = getInputAmount(commandString[2]);
+                    if (price > 0)
+                    {
+                        stockQuery.update(client, commandString[1], price);
+                        System.out.println("Price of " + commandString[1] + " updated to " + price + " .");
+                    }
+                    else
+                    {
+                        System.out.println("Enter price greater than zero");
+                    }
+                }
+                catch (NumberFormatException ne)
+                {
+                    printWarning(commandString[0]);
+                }
+                catch (Exception ex)
+                {
+                    printMessage(ex.getMessage());
+                }
+            }
+            else
+            {
+                printWarning(commandString[0]);
+            }
+        }
+        else
+        {
+            printloginMessage();
+        }
+    }
+
+    /**
+     * Handles the user command
+     * <p/>
+     * @param commandString
+     * <p/>
+     * @throws RemoteException
+     */
+    private void login(String[] commandString) throws RemoteException
+    {
+        if (commandString.length == 2)
+        {
+            client = authentication.init(commandString[1], isAdmin);
+            if (!client.isAdmin())
+            {
+                printMessage("Your Balance is :" + client.getBalance());
+            }
+        }
+        else
+        {
+            printWarning(commandString[0]);
+        }
+    }
+
+    /**
+     * Initializes the Interaction
+     * <p/>
      * @throws RemoteException
      */
     public void init() throws RemoteException
@@ -91,198 +335,30 @@ public class InteractionManager
             switch (commandString[0])
             {
                 case "query":
-                    if (isLoggedIn())
-                    {
-                        if (commandString.length == 2)
-                        {
-                            try
-                            {
-                                System.out.println(stockQuery.query(client, commandString[1]));
-                            }
-                            catch (Exception ex)
-                            {
-
-                                printMessage(ex.getMessage());
-                            }
-                        }
-                        else
-                        {
-                            printWarning(commandString[0]);
-                        }
-                    }
-                    else
-                    {
-                        printloginMessage();
-                    }
+                    query(commandString);
                     break;
 
                 case "list":
-                    if (isLoggedIn())
-                    {
-                        if (commandString.length == 1 && !client.isAdmin())
-                        {
-
-                            List<Stock> list = stockQuery.list(client);
-                            if (list.size() > 0)
-                            {
-                                for (Stock stock : list)
-                                {
-                                    System.out.println(stock);
-                                }
-                            }
-                            else
-                            {
-                                printMessage("No records found.");
-                            }
-
-
-                        }
-                        else
-                        {
-                            printWarning(commandString[0]);
-                        }
-                    }
-                    else
-                    {
-                        printloginMessage();
-                    }
+                    list(commandString);
 
                     break;
 
                 case "buy":
-                    if (isLoggedIn())
-                    {
-                        if (commandString.length == 3 && !client.isAdmin())
-                        {
-
-                            try
-                            {
-                                quantity = getInteger(commandString[2]);
-                                if (quantity > 0)
-                                {
-                                    client = stockQuery.buy(client, commandString[1], quantity);
-                                    System.out.println(Integer.toString(quantity) + " " + commandString[1] + " bought.");
-                                    System.out.println(" Your current balance is $" + client.getBalance() + " .");
-                                }
-                                else
-                                {
-                                    System.out.println("Enter quantity greater than zero.");
-                                }
-                            }
-                            catch (NumberFormatException ne)
-                            {
-                                printWarning(commandString[0]);
-                            }
-                            catch (Exception ex)
-                            {
-                                printMessage(ex.getMessage());
-                            }
-
-                        }
-                        else
-                        {
-                            printWarning(commandString[0]);
-                        }
-                    }
-                    else
-                    {
-                        printloginMessage();
-                    }
+                    buy(commandString);
 
                     break;
 
                 case "sell":
-                    if (isLoggedIn())
-                    {
-                        if (commandString.length == 3 && !client.isAdmin())
-                        {
-
-                            try
-                            {
-                                quantity = getInteger(commandString[2]);
-                                if (quantity > 0)
-                                {
-                                    client = stockQuery.sell(client, commandString[1], quantity);
-                                    System.out.println(Integer.toString(quantity) + " " + commandString[1] + " sold.");
-                                    System.out.println(" Your current balance is $" + client.getBalance() + " .");
-                                }
-                                else
-                                {
-                                    System.out.println("Enter quantity great than zero.");
-                                }
-                            }
-                            catch (NumberFormatException ne)
-                            {
-                                printWarning(commandString[0]);
-                            }
-                            catch (Exception ex)
-                            {
-                                printMessage(ex.getMessage());
-                            }
-                        }
-                        else
-                        {
-                            printWarning(commandString[0]);
-                        }
-                    }
-                    else
-                    {
-                        printloginMessage();
-                    }
+                    sell(commandString);
 
                     break;
 
                 case "update":
-                    if (isLoggedIn())
-                    {
-                        if (commandString.length == 3 && client.isAdmin())
-                        {
-                            try
-                            {
-                                double price = getInputAmount(commandString[2]);
-                                if (price > 0)
-                                {
-                                    stockQuery.update(client, commandString[1], price);
-                                    System.out.println("Price of " + commandString[1] + " updated to " + price + " .");
-                                }
-                                else
-                                {
-                                    System.out.println("Enter price greater than zero");
-                                }
-                            }
-                            catch (NumberFormatException ne)
-                            {
-                                printWarning(commandString[0]);
-                            }
-                            catch (Exception ex)
-                            {
-                                printMessage(ex.getMessage());
-                            }
-                        }
-                        else
-                        {
-                            printWarning(commandString[0]);
-                        }
-                    }
-                    else
-                    {
-                        printloginMessage();
-                    }
+                    update(commandString);
 
                     break;
                 case "user":
-                    if (commandString.length == 2)
-                    {
-                        client = authentication.init(commandString[1], isAdmin);
-                        if (!client.isAdmin())
-                        {
-                            printMessage("Your Balance is :" + client.getBalance());
-                        }
-                    }
-                    else
-                    {
-                        printWarning(commandString[0]);
-                    }
+                    login(commandString);
                     break;
                 case "help":
                     if (commandString.length == 1)
@@ -299,12 +375,13 @@ public class InteractionManager
 
                     break;
             }
-
-
         }
         while (!isExit);
     }
 
+    /**
+     * Print command options
+     */
     public void printPrompt()
     {
         System.out.println("-----------------------------------------------");
